e7d121426392517bfcc7f75c00ab98a1a6a89b05
Wojciech Dobija <dobijaw@gmail.com>
2020-01-24 00:09:22 +0100

    add simple Webpack configuration with Babel

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    filename: 'js/bundle.js'
+  },
+  devServer: {
+    contentBase: './docs'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/html/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'index.css'
+    })
+  ],
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: [/.css$|.scss$/],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|wav|mp3)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'img/[name].[ext]',
+        }
+      },
+    ]
+  }
+}
