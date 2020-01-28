1fd4e3a8e0e506c17ea4a0a4f217e685298108c9
Wojciech Dobija <dobijaw@gmail.com>
2020-01-28 20:59:19 +0100

    update footer and header styles for all platform

--- src/js/index.js
+++ src/js/index.js
@@ -1,78 +1,78 @@
 import '../scss/index.scss';
-import {
-    state,
-    resetAllState,
-    resetStateChoice
-} from './state';
-import {
-    DOMelements,
-    DOMclasses
-} from './base';
+// import {
+//     state,
+//     resetAllState,
+//     resetStateChoice
+// } from './state';
+// import {
+//     DOMelements,
+//     DOMclasses
+// } from './base';
 
-import playerChoiceView from './view/playerChoiceView';
-import showResult from './view/showResult';
-import showGameBoard from './view/showGameBoard';
-import PlayerChoice from './model/PlayerChoice';
-import Winner from './model/Winner';
-import AIChoice from './model/AIChoice';
-import {
-    resetUI,
-    resetBoxUI
-} from './view/resetUI';
-import updateScore from './view/updateScore';
+// import playerChoiceView from './view/playerChoiceView';
+// import showResult from './view/showResult';
+// import showGameBoard from './view/showGameBoard';
+// import PlayerChoice from './model/PlayerChoice';
+// import Winner from './model/Winner';
+// import AIChoice from './model/AIChoice';
+// import {
+//     resetUI,
+//     resetBoxUI
+// } from './view/resetUI';
+// import updateScore from './view/updateScore';
 
-const init = () => {
-    resetUI();
-    resetAllState();
-    showGameBoard();
-    state.isGameBoard = true;
-}
+// const init = () => {
+//     resetUI();
+//     resetAllState();
+//     showGameBoard();
+//     state.isGameBoard = true;
+// }
 
-init();
-
-
-DOMelements.symbols.forEach((box, index, arr) => box.addEventListener('click', ({
-    currentTarget
-}) => {
-    if (state.isPopupOpen) return;
-
-    playerChoiceView(currentTarget, index, arr);
-    new PlayerChoice(currentTarget);
-}));
-
-DOMelements.playBtn.addEventListener('click', () => {
-    if (state.isPopupOpen) return;
-
-    if (state.isGameBoard) {
-        if (state.playerChoice === '') return;
-        new AIChoice();
-        const winner = new Winner();
-        const scoreStatus = winner.checkStatusWin();
-        winner.updateStateScore(scoreStatus);
-        updateScore(scoreStatus);
-        const copyStatus = winner.renderWinnerStatus(scoreStatus);
-        showResult(copyStatus, scoreStatus);
-        resetBoxUI();
-        resetStateChoice();
-
-        state.isGameBoard = false;
-    } else {
-        showGameBoard();
-        state.isGameBoard = true;
-    }
-})
+// init();
 
 
-DOMelements.reset.addEventListener('click', () => {
-    DOMelements.popup.classList.add(DOMclasses.popupActive);
-    state.isPopupOpen = true;
+// DOMelements.symbols.forEach((box, index, arr) => box.addEventListener('click', ({
+//     currentTarget
+// }) => {
+//     if (state.isPopupOpen) return;
 
-    DOMelements.popupBtns.forEach(btn => btn.addEventListener('click', () => {
-        DOMelements.popup.classList.remove(DOMclasses.popupActive);
-        state.isPopupOpen = false;
+//     playerChoiceView(currentTarget, index, arr);
+//     new PlayerChoice(currentTarget);
+// }));
 
-        if (btn.dataset.btn === 'no') return;
-        init();
+// DOMelements.playBtn.addEventListener('click', () => {
+//     if (state.isPopupOpen) return;
 
-    }))
-})
+//     if (state.isGameBoard) {
+//         if (state.playerChoice === '') return;
+//         new AIChoice();
+//         const winner = new Winner();
+//         const scoreStatus = winner.checkStatusWin();
+//         winner.updateStateScore(scoreStatus);
+//         updateScore(scoreStatus);
+//         const copyStatus = winner.renderWinnerStatus(scoreStatus);
+//         showResult(copyStatus, scoreStatus);
+//         resetBoxUI();
+//         resetStateChoice();
+
+//         state.isGameBoard = false;
+//     } else {
+//         showGameBoard();
+//         state.isGameBoard = true;
+//     }
+// })
+
+
+// DOMelements.reset.addEventListener('click', () => {
+//     DOMelements.popup.classList.add(DOMclasses.popupActive);
+//     state.isPopupOpen = true;
+
+//     DOMelements.popupBtns.forEach(btn => btn.addEventListener('click', () => {
+//         DOMelements.popup.classList.remove(DOMclasses.popupActive);
+//         state.isPopupOpen = false;
+
+//         if (btn.dataset.btn === 'no') return;
+//         init();
+
+//     }))
+// })
