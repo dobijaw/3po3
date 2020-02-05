8b88ed4f08103792351335b9e276b841cc965642
Wojciech Dobija <dobijaw@gmail.com>
2020-02-05 21:30:20 +0100

    remove first implementation of the game, starts with subscriber that observe game variants

--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,69 @@
+
+
+// import { state } from './state';
+
+
+// import { Variant } from './model/Variant';
+// import Winner from './model/Winner';
+
+// import getMessageView from './view/getMessageView';
+
+// import { winnerView, updateScoreView, updateHeadlineView } from './view/winnerView';
+// import { resetSummaryView } from './view/summaryView';
+// import { changeGameVariant, hideActualVariantBtn } from './view/changeGameVariant';
+// import playAgainView from './view/playAgainView';
+// import showResetPopUpView from './view/showResetPopUpView';
+
+// const newGame = () => {
+//     state.keyBlocked = false;
+//     const gameVariants = new Variant();
+//     const currentVariant = gameVariants.getVariantGame(state.gameVariant);
+
+//     updateScoreView();
+
+//     getMessageView(currentVariant.getMessage());
+//     currentVariant.renderGameBoard();
+//     currentVariant.aiChoice();
+
+//     currentVariant.subscribe(() => {
+//         const aiSymbol = currentVariant.stopSymbolInterval();
+//         currentVariant.saveChoiceInState(aiSymbol);
+
+//         const winner = new Winner();
+//         const result = winner.saveWinner();
+
+//         winnerView(result);
+//         updateHeadlineView(result);
+//         state.enterBlocked = false;
+
+//         playAgainView(newGame);
+//     })
+
+//     currentVariant.playerChoice();
+// }
+
+// const initGame = () => {
+//     resetSummaryView();
+//     changeGameVariant(newGame);
+//     hideActualVariantBtn();
+//     showResetPopUpView(newGame);
+
+//     newGame();
+// }
+
+
+
+// document.addEventListener('DOMContentLoaded', () => initGame());
+
 import '../scss/index.scss';
 
-import { state } from './state';
+import { VariantFactory } from './model/VariantFactory';
+import { GameVariantsUI } from './model/GameVariantsUI';
 
+const variant = new VariantFactory();
+const gameVariants = new GameVariantsUI();
 
-import { Variant } from './model/Variant';
-import Winner from './model/Winner';
+gameVariants.subscribe(selectedVariant => {
+    const currentVariant = variant.getVariantGame(selectedVariant);
 
-import getMessageView from './view/getMessageView';
-
-import { winnerView, updateScoreView, updateHeadlineView } from './view/winnerView';
-import { resetSummaryView } from './view/summaryView';
-import { changeGameVariant, hideActualVariantBtn } from './view/changeGameVariant';
-import playAgainView from './view/playAgainView';
-import showResetPopUpView from './view/showResetPopUpView';
-
-const newGame = () => {
-    state.keyBlocked = false;
-    const gameVariants = new Variant();
-    const currentVariant = gameVariants.getVariantGame(state.gameVariant);
-
-    updateScoreView();
-
-    getMessageView(currentVariant.getMessage());
-    currentVariant.renderGameBoard();
-    currentVariant.aiChoice();
-
-    currentVariant.subscribe(() => {
-        const aiSymbol = currentVariant.stopSymbolInterval();
-        currentVariant.saveChoiceInState(aiSymbol);
-
-        const winner = new Winner();
-        const result = winner.saveWinner();
-
-        winnerView(result);
-        updateHeadlineView(result);
-        state.enterBlocked = false;
-
-        playAgainView(newGame);
-    })
-
-    currentVariant.playerChoice();
-}
-
-const initGame = () => {
-    resetSummaryView();
-    changeGameVariant(newGame);
-    hideActualVariantBtn();
-    showResetPopUpView(newGame);
-
-    newGame();
-}
-
-
-
-document.addEventListener('DOMContentLoaded', () => initGame());
+})
