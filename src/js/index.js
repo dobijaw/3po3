c87b766b50fcd5dca1e05842cc1c2964fcc5ad16
Wojciech Dobija <dobijaw@gmail.com>
2020-02-06 22:11:50 +0100

    add reset Score and State functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,16 @@
 import "../scss/index.scss";
-import { state } from "./state";
 
 import { VariantFactory } from "./model/VariantFactory";
 import { GameVariantsObserve } from "./model/GameVariantsObserve";
 import { GamePlayControler } from "./model/GamePlayControler";
 import { GameBoard } from "./model/GameBoard";
+import { Scores } from "./model/Scores";
 
 const variant = new VariantFactory();
 const gameVariants = new GameVariantsObserve();
 const gamePlayControler = new GamePlayControler();
 const gameBoard = new GameBoard();
+const scores = new Scores();
 
 gameVariants.subscribe(selectedVariant => {
   const currentVariant = variant.getVariantGame(selectedVariant);
@@ -25,4 +26,6 @@ gameVariants.subscribe(selectedVariant => {
   });
 });
 
+scores.resetScoresInState();
+scores.resetScoresView();
 gameVariants.initFirstScreen();
