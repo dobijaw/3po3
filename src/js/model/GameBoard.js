e5fb6b025c7c1636c151c4915bc695740a32406e
Wojciech Dobija <dobijaw@gmail.com>
2020-02-07 11:56:04 +0100

    prettier all files

--- src/js/model/GameBoard.js
+++ src/js/model/GameBoard.js
@@ -1,39 +1,38 @@
-import { DOMelements } from '../base';
+import { DOMelements } from "../base";
 
 export class GameBoard {
-    constructor() {
-        this.currentVariant = null;
-    }
+  constructor() {
+    this.currentVariant = null;
+  }
 
-    clearGameBoardView() {
-        DOMelements.board.textContent = '';
-    }
+  clearGameBoardView() {
+    DOMelements.board.textContent = "";
+  }
 
-    renderGameBoardView(variant) {
-        this.clearGameBoardView();
+  renderGameBoardView(variant) {
+    this.clearGameBoardView();
 
-        DOMelements.boardTemplates.forEach(template => {
-            if (template.dataset.board !== variant) return;
+    DOMelements.boardTemplates.forEach(template => {
+      if (template.dataset.board !== variant) return;
 
-            const clon = template.content.cloneNode(true);
-            DOMelements.board.appendChild(clon);
+      const clon = template.content.cloneNode(true);
+      DOMelements.board.appendChild(clon);
+    });
+  }
 
-        });
-    }
+  renderMessageView() {
+    const message = this.currentVariant.getMessage();
 
-    renderMessageView() {
-        const message = this.currentVariant.getMessage();
+    DOMelements.messageHeadline.textContent = message.headline;
+    DOMelements.messageCopy.textContent = message.copy;
+  }
 
-        DOMelements.messageHeadline.textContent = message.headline;
-        DOMelements.messageCopy.textContent = message.copy;
-    }
+  startGame() {
+    this.renderMessageView();
+    this.renderGameBoardView(this.currentVariant.name);
+  }
 
-    startGame() {
-        this.renderMessageView();
-        this.renderGameBoardView(this.currentVariant.name);
-    }
-
-    changeCurrentVariant(variant) {
-        this.currentVariant = variant;
-    }
+  changeCurrentVariant(variant) {
+    this.currentVariant = variant;
+  }
 }
